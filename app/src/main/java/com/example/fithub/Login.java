31e5d32bb3d6b31e16474d1ff6e87249547b5935
notlydia <yougotfluffy@gmail.com>
2021-05-06 23:57:37 -0400

    Add Login switch to Register

--- app/src/main/java/com/example/fithub/Login.java
+++ app/src/main/java/com/example/fithub/Login.java
@@ -2,15 +2,34 @@ package com.example.fithub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView registerPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        registerPage = (TextView) findViewById(R.id.reg);
+        registerPage.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.reg:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
     }
 }
