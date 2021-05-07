92e58c95f4f93e7fa7472bcb94354abc48b8a95b
dibakar7951 <64855725+dibakar7951@users.noreply.github.com>
2021-05-06 23:00:29 -0400

    Dibakar (#8)
    * added checklist
    
    * added checklist
    
    * added checklist

--- app/src/main/java/com/example/fithub/SplashScreen.java
+++ app/src/main/java/com/example/fithub/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(this,MainActivity.class));
+        startActivity(new Intent(this,user_preference_checklist.class));
         finish();
     }
 }
